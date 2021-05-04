3134dc00ecbc62e78f282d63cdcd72ee153ab700
Mihaela Rete <mihaela.rete@takeofflabs.com>
2021-05-04 13:56:47 +0300

    change uid and secret

--- config/initializers/a_env.rb
+++ config/initializers/a_env.rb
@@ -2,5 +2,5 @@ ENV['LOGIN_URL'] ||= "https://whs-dev.herokuapp.com/api/v1/users/login.json"
 ENV['SEARCH_GOLFER_URL'] ||= "https://whs-dev.herokuapp.com/api/v1/golfers.json?page=1&per_page=25&global_search=true&sorting_criteria=full_name&order=asc"
 ENV['VENDOR_USER_EMAIL'] ||= 'sampleappclient@noemail.com'
 ENV['VENDOR_USER_PASSWORD'] ||= '123456'
-ENV["OAUTH_ID"] ||= 'nJG7Oopi3ddgblixAlUGxmj9H9Ef1A7ZD5PVLimLeck'
-ENV["OAUTH_SECRET"] ||= 'S_aBXmqAkYWbXqNwvhcCSz50k_IjhWDdkFtVoy4M8YE'
+ENV["OAUTH_ID"] ||= 'Kv7IqaAS0NgXvB8ogZh_eKaC1N0VlBhNHC5Wd9xQ5iA'
+ENV["OAUTH_SECRET"] ||= '2HLuYiSPjTCf7MiZye45nd6OyYN0R8SocxkHMZUaTDg'
