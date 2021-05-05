ec3b953e480942aa1e6872519fa1a9ecec79cdf0
Mihaela Rete <mihaela.rete@takeofflabs.com>
2021-05-05 12:37:51 +0300

    add guardian details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,15 +10,26 @@ class PagesController < ApplicationController
      @result_from_login = HTTParty.post(ENV['LOGIN_URL'].to_str,
         :body => { :user =>{ :email => ENV['VENDOR_USER_EMAIL'] , :password => ENV['VENDOR_USER_PASSWORD'] }}.to_json,
         :headers => { 'Content-Type' => 'application/json' } )
-     query = { "search" => object_id}
      barear_token = @result_from_login["token"]
 
-     @result_form_golfer_search = HTTParty.get(ENV['SEARCH_GOLFER_URL'].to_str,
+     if object_type == "golfer"
+       query = { "search" => object_id}
+       @result_form_golfer_search = HTTParty.get(ENV['SEARCH_GOLFER_URL'].to_str,
+           :query => query,
+           :headers => {"Authorization" => "Bearer #{barear_token}" , 'Content-Type' => 'application/json' } )
+       result = JSON.parse @result_form_golfer_search.body.gsub('=>', ':')
+       @ghin_number = result["golfers"].first["ghin"]
+       @golfer_name = result["golfers"].first["first_name"] + " " + result["golfers"].first["last_name"]
+       @golfer_hi = result["golfers"].first["hi_display"]
+     else
+       query = { "source" => "GHINcom"}
+       @object_id = object_id
+       @result_form_guardian_search = HTTParty.get((ENV['SEARCH_GUARDIAN_URL'] + "/" + "#{object_id}" + ".json").to_str,
          :query => query,
          :headers => {"Authorization" => "Bearer #{barear_token}" , 'Content-Type' => 'application/json' } )
-     result = JSON.parse @result_form_golfer_search.body.gsub('=>', ':')
-     @ghin_number = result["golfers"].first["ghin"]
-     @golfer_name = result["golfers"].first["first_name"] + " " + result["golfers"].first["last_name"]
-     @golfer_hi = result["golfers"].first["hi_display"]
+       result = JSON.parse @result_form_guardian_search.body.gsub('=>', ':')
+       @guardian_id = result["guardian"]["id"]
+       @guardian_name = result["guardian"]["first_name"] + " " + result["guardian"]["last_name"]
+     end
   end
 end
--- config/initializers/a_env.rb
+++ config/initializers/a_env.rb
@@ -1,5 +1,6 @@
 ENV['LOGIN_URL'] ||= "https://whs-dev.herokuapp.com/api/v1/users/login.json"
 ENV['SEARCH_GOLFER_URL'] ||= "https://whs-dev.herokuapp.com/api/v1/golfers.json?page=1&per_page=25&global_search=true&sorting_criteria=full_name&order=asc"
+ENV['SEARCH_GUARDIAN_URL'] ||= "https://whs-dev.herokuapp.com/api/v1/guardians"
 ENV['VENDOR_USER_EMAIL'] ||= 'sampleappclient@noemail.com'
 ENV['VENDOR_USER_PASSWORD'] ||= '123456'
 ENV["OAUTH_ID"] ||= 'Kv7IqaAS0NgXvB8ogZh_eKaC1N0VlBhNHC5Wd9xQ5iA'
